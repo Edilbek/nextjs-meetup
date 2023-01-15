c69fc14740737b9dce369e34670a40453d701b3a
Edilbek <edil.talantbekov@gmail.com>
2023-01-15 22:03:30 +0600

    add meetupdetail component

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import MeetupDetails from '../../components/meetups/MeetupDetails';
+
+const MeetupDetailsPage = () => {
+  return (
+    <MeetupDetails 
+      image='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png'
+      title='First meetup'
+      address='Some Street 5'
+      description='This is a first meetup'
+    />
+  )
+}
+
+export default MeetupDetailsPage;
