860f8a04b06d0e7465fd639e9e8d51a635a70b6f
Edilbek <edil.talantbekov@gmail.com>
2023-02-02 11:42:33 +0600

    post request to create a record in mongodb

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,33 @@
-import React from 'react'
-
 import MeetupList from '../components/meetups/MeetupList';
 
 const DUMMY_MEETUPS = [
   {
     id: 'm1',
-    title: 'A first demo',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png',
-    address: 'Some address 5',
-    description: 'Some description'
+    title: 'A First Meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Some address 5, 12345 Some City',
+    description: 'This is a first meetup!',
   },
   {
     id: 'm2',
-    title: 'A second demo',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png',
-    address: 'Some address 5',
-    description: 'Some description'
+    title: 'A Second Meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Some address 10, 12345 Some City',
+    description: 'This is a second meetup!',
   },
-  {
-    id: 'm3',
-    title: 'A third demo',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png',
-    address: 'Some address 5',
-    description: 'Some description'
-  }
-]
+];
 
-const HomePage = (props) => {
-  return (
-    <MeetupList meetups={props.meetups} />
-  )
+function HomePage(props) {
+  return <MeetupList meetups={props.meetups} />;
 }
 
-// export const getServerSideProps = async (context) => {
+// export async function getServerSideProps(context) {
 //   const req = context.req;
 //   const res = context.res;
 
-//   //   // fetch data from an API
+//   // fetch data from an API
 
 //   return {
 //     props: {
@@ -45,15 +36,14 @@ const HomePage = (props) => {
 //   };
 // }
 
-// export const getStaticProps = async () => {
-//   // fetch data from an API
-
-//   return {
-//     props: {
-//       meetups: DUMMY_MEETUPS
-//     },
-//     revalidate: 1,
-//   };
-// }
+export async function getStaticProps() {
+  // fetch data from an API
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS
+    },
+    revalidate: 1
+  }; 
+}
 
 export default HomePage;
